af3953c6d0666efe014b64a20d849bbc9910f0f2
cuentapracticas2005 <cuentapracticas@gmail.com>
2025-08-15 17:20:11 -0500

    actualizacion dia viernes page

--- modals/modal_agregar_archivo/m_agregar_archivo.js
+++ modals/modal_agregar_archivo/m_agregar_archivo.js
@@ -1,14 +1,15 @@
 // Función para cargar el modal desde m_agregar_archivo.html y agregarlo al DOM
-document.addEventListener("DOMContentLoaded", function () {
-    fetch('../modals/modal_agregar_archivo/m_agregar_archivo.html')
-        .then(response => response.text())
+document.addEventListener("DOMContentLoaded", function () { //Garantiza que el modal se crague despues de que la pagina este lista
+    fetch('../modals/modal_agregar_archivo/m_agregar_archivo.html') //Se usa "fetch() para cargar el contenido desde un archivo HTML externo"
+        //Realiza una solicitud HTTP para obtener el contenido del modal
+        .then(response => response.text()) //Convierte la respuesta en texto
         .then(html => {
             // Insertar el modal en el contenedor
             const container = document.getElementById('modalContainer');
             if (container) {
                 container.innerHTML = html;
             }
-        });
+        }); // Funcion: Busca un elemento con ID: modalContainer, inserta el contenido del modal en el contenedor si es que este existe
 });
 
 // Función para mostrar el modal (puedes llamarla desde un botón)
