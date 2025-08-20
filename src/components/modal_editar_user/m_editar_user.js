8ad7662387b7807b4f3619666655a13a0fdb3171
cuentapracticas2005 <cuentapracticas@gmail.com>
2025-08-20 08:33:50 -0500

    actualizacion6

--- src/components/modal_editar_user/m_editar_user.js
+++ src/components/modal_editar_user/m_editar_user.js
@@ -2,5 +2,18 @@ document.addEventListener("DOMContentLoaded", function (){
     fetch('../../src/components/modal_editar_user/m_editar_user.html')
     .then(Response => Response.text())
     .then(html => {
-        });
+
+        const container = document.getElementById('modalContainerEditar');
+        if (container) {
+            container.innerHTML = html;
+        }
+    });
 })
+
+//funcion para mostrar el modal
+function mostrarModalEditar() {
+    const modal = document.getElementById('modalEditar');
+    if (modal) {
+        modal.classList.remove('hidden');
+    }
+}
